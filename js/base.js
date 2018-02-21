2fd0f869171000f68590bb3440ff3611d61a34be
iriscsun <iris.sun@nike.com>
2018-02-20 17:51:11 -0800

    add styling to credentials page and add a form to post a new recipe

--- js/base.js
+++ js/base.js
@@ -5,3 +5,18 @@ $(document).ready(function () {
 	$('#nav').load('../nav.html');
 
 });
+
+var counter = 1;
+var limit = 15;
+
+function addInput(divName){
+     if (counter == limit)  {
+          alert("You have reached the limit of adding " + counter + " inputs");
+     }
+     else {
+          var newdiv = document.createElement('span');
+          newdiv.innerHTML ="<br><input class='field' type='text' name='ingredients[]' placeholder='Ingredient " + (counter + 1) + "'>";
+          document.getElementById(divName).appendChild(newdiv);
+          counter++;
+     }
+}
