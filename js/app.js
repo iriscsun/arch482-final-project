ce04c91ddfb8fb9f3e0a15bf0195c11347f411a2
zubinchopra <zubinc@uw.edu>
2018-03-03 00:30:50 -0800

    Hooked up firebase backend service

---
+++ js/app.js
@@ -0,0 +1,67 @@
+// Initialize Firebase
+let config = {
+    apiKey: "AIzaSyAMTv6u00ZDTiILop8w4NW7XNbI7624oZ4",
+    authDomain: "arch-482.firebaseapp.com",
+    databaseURL: "https://arch-482.firebaseio.com",
+    projectId: "arch-482",
+    storageBucket: "arch-482.appspot.com",
+    messagingSenderId: "970186204682"
+};
+firebase.initializeApp(config);
+
+let signup_form = document.getElementById('signup-form');
+let login_form = document.getElementById('login-form');
+
+login_form.addEventListener('submit', e => {
+    
+    e.preventDefault();
+
+    let email = document.getElementById('login-email').value;
+    let password = document.getElementById('login-password').value;
+
+    firebase.auth().signInWithEmailAndPassword(email, password)
+        .then(user => {
+            console.log('user ' + user.uid + ' logged in');
+        })
+        .catch(err => {
+            console.log(err);
+        });
+
+});
+
+signup_form.addEventListener('submit', e => {
+    
+    e.preventDefault();
+
+    let email = document.getElementById('email').value;
+    let password = document.getElementById('password').value;
+
+    console.log(email);
+    console.log(password);
+
+    firebase.auth().createUserWithEmailAndPassword(email, password)
+        .then(user => {
+            console.log('user ' + user.uid + ' created');
+        })
+        .catch(err => {
+            console.log(err);
+        });
+
+});
+
+firebase.auth().onAuthStateChanged(user => {
+    if(user) {
+        console.log('user logged in!');
+    }
+    else {
+        console.log('no user logged in!');
+    }
+});
+
+window.onload = () => {
+    let signout = document.getElementById('signout');
+    signout.style.cursor = "pointer";
+    signout.addEventListener('click', () => {
+        firebase.auth().signOut();
+    });
+}
