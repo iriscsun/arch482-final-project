4ffb2a73984c81986db9746105ab8a9dc05a2d49
zubinchopra <zubinc@uw.edu>
2018-03-03 11:08:02 -0800

    Signout button shows only when user is logged in.

--- js/app.js
+++ js/app.js
@@ -9,59 +9,63 @@ let config = {
 };
 firebase.initializeApp(config);
 
-let signup_form = document.getElementById('signup-form');
-let login_form = document.getElementById('login-form');
-
-login_form.addEventListener('submit', e => {
-    
-    e.preventDefault();
-
-    let email = document.getElementById('login-email').value;
-    let password = document.getElementById('login-password').value;
-
-    firebase.auth().signInWithEmailAndPassword(email, password)
-        .then(user => {
-            console.log('user ' + user.uid + ' logged in');
-        })
-        .catch(err => {
-            console.log(err);
-        });
-
-});
-
-signup_form.addEventListener('submit', e => {
-    
-    e.preventDefault();
-
-    let email = document.getElementById('email').value;
-    let password = document.getElementById('password').value;
-
-    console.log(email);
-    console.log(password);
-
-    firebase.auth().createUserWithEmailAndPassword(email, password)
-        .then(user => {
-            console.log('user ' + user.uid + ' created');
-        })
-        .catch(err => {
-            console.log(err);
-        });
-
-});
-
-firebase.auth().onAuthStateChanged(user => {
-    if(user) {
-        console.log('user logged in!');
-    }
-    else {
-        console.log('no user logged in!');
-    }
-});
-
 window.onload = () => {
+
+    let signup_form = document.getElementById('signup-form');
+    let login_form = document.getElementById('login-form');
     let signout = document.getElementById('signout');
-    signout.style.cursor = "pointer";
-    signout.addEventListener('click', () => {
-        firebase.auth().signOut();
+    
+    login_form.addEventListener('submit', e => {
+        
+        e.preventDefault();
+    
+        let email = document.getElementById('login-email').value;
+        let password = document.getElementById('login-password').value;
+    
+        firebase.auth().signInWithEmailAndPassword(email, password)
+            .then(user => {
+                console.log('user ' + user.uid + ' logged in');
+            })
+            .catch(err => {
+                console.log(err);
+            });
+    
     });
+    
+    signup_form.addEventListener('submit', e => {
+        
+        e.preventDefault();
+    
+        let email = document.getElementById('email').value;
+        let password = document.getElementById('password').value;
+    
+        console.log(email);
+        console.log(password);
+    
+        firebase.auth().createUserWithEmailAndPassword(email, password)
+            .then(user => {
+                console.log('user ' + user.uid + ' created');
+            })
+            .catch(err => {
+                console.log(err);
+            });
+    
+    });
+    
+    firebase.auth().onAuthStateChanged(user => {
+        if(user) {
+            console.log('user logged in!');
+            signout.style.display = "block";
+            signout.style.cursor = "pointer";
+            signout.addEventListener('click', () => {
+                firebase.auth().signOut();
+            });
+        }
+        else {
+            console.log('no user logged in!');
+            signout.style.display = "none";
+        }
+    });
+
 }
+
