7ae6e809d04f99ef8fda15cdd251262c0a995b3d
zubinchopra <zubinc@uw.edu>
2018-03-05 01:36:02 -0800

    Cleaned up the post-recipe code.

--- js/post-recipe.js
+++ js/post-recipe.js
@@ -15,47 +15,44 @@ let myRecipes = [];
 window.onload = () => {
 
     let signout = document.getElementById('signout');
-    //let recipeForm = document.getElementById('recipe-form');
-		//recipeForm.addEventListener('submit', e => {
-		//e.preventDefault();
 
 		let submitBtn = document.getElementById('submit');
 		submitBtn.addEventListener('click', function() {
 
-        let ing = [];
+            let ing = [];
 
-        let name = document.getElementById('name').value;
-        let servingSize = $("#serving-size option:selected").text();
-        let recipeTime = $("input[name='inlineRadioOptions']:checked").next()[0].innerText || "";
-        let cookingDirections = document.getElementsByClassName('cooking-directions')[0].value;
-        let tags = document.getElementById('tags').value;
-        let ingredients = document.getElementsByClassName('ingredient');
+            let name = document.getElementById('name').value;
+            let servingSize = $("#serving-size option:selected").text();
+            let recipeTime = $("input[name='inlineRadioOptions']:checked").next()[0].innerText || "";
+            let cookingDirections = document.getElementsByClassName('cooking-directions')[0].value;
+            let tags = document.getElementById('tags').value;
+            let ingredients = document.getElementsByClassName('ingredient');
 
-        for(let i = 0; i < ingredients.length; i++) {
-            ing.push(ingredients[i].value);
-        }
+            for(let i = 0; i < ingredients.length; i++) {
+                ing.push(ingredients[i].value);
+            }
 
-        let postData = {
-            name: name,
-            servingSize: servingSize,
-            recipeTime: recipeTime,
-            cookingDirections: cookingDirections,
-            tags: tags,
-            ingredients: ing
-        }
+            let postData = {
+                name: name,
+                servingSize: servingSize,
+                recipeTime: recipeTime,
+                cookingDirections: cookingDirections,
+                tags: tags,
+                ingredients: ing
+            }
 
-        let recipekey = firebase.database().ref().child('recipes').push().key;
-        myRecipes.push(recipekey);
-        let recipe_updates = {};
-        let user_updates = {};
-        recipe_updates['/recipes/' + recipekey] = postData;
-        user_updates['/users/' + userid + '/recipes'] = myRecipes;
-        firebase.database().ref().update(recipe_updates);
-        firebase.database().ref().update(user_updates);
+            let recipekey = firebase.database().ref().child('recipes').push().key;
+            myRecipes.push(recipekey);
+            let recipe_updates = {};
+            let user_updates = {};
+            recipe_updates['/recipes/' + recipekey] = postData;
+            user_updates['/users/' + userid + '/recipes'] = myRecipes;
+            firebase.database().ref().update(recipe_updates);
+            firebase.database().ref().update(user_updates);
 
-        firebase.database().ref('/recipes/').on('child_added', (snapshot) => {
+            firebase.database().ref('/recipes/').on('child_added', (snapshot) => {
 
-            location.reload();
+                location.reload();
 
         });
 
